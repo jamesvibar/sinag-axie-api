fbd122913595f34a38735985e8067e8f11776cb6
James Raphael Vibar <vibarjamess@gmail.com>
2021-10-19 06:24:13 +0800

    feat: 🎸 populate todayslp, yesterday slp on find one

--- api/apprentices/services/apprentices.js
+++ api/apprentices/services/apprentices.js
@@ -1,4 +1,5 @@
 "use strict";
+const ObjectId = require("mongoose").Types.ObjectId;
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
@@ -20,5 +21,26 @@ module.exports = {
       ]);
 
     return documents;
+  },
+  async findOne(params, populate = []) {
+    const id = params?.id;
+
+    if (!ObjectId.isValid(id)) {
+      throw strapi.errors.notFound("Not found");
+    }
+
+    const document = await strapi
+      .query("apprentices")
+      .model.findOne({ _id: id })
+      .populate([
+        {
+          path: "account",
+          select: "-daily_slps",
+          populate: [{ path: "today_slp" }, { path: "yesterday_slp" }],
+        },
+        ...populate.map((p) => ({ path: p })),
+      ]);
+
+    return document;
   },
 };
